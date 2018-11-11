1c5eda16f04f62c7ab9cd5d3f53be65a66446e57
oioilalala <35413266+oioilalala@users.noreply.github.com>
2018-11-10 16:06:21 -0800

    Update mmm_mpi.c

--- mmm_mpi.c
+++ mmm_mpi.c
@@ -72,55 +72,67 @@ int main(int argc, char **argv) {
     int n, numtasks, taskid, tasksz, tid, allsz;
     struct timespec t1, t2;
     double time_pass, time_sec, time_nsec;
-    checkArgc(argc);
-    readArgv(argv, &n);
-    double *A = malloc(n * n * sizeof(double));
-    double *B = malloc(n * n * sizeof(double));
-    double *C = malloc(n * n * sizeof(double));
     
     MPI_Init(&argc,&argv);
     MPI_Status status;
     MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
-    
-    tasksz = n * n / numtasks;
-    allsz = n * n;
+   
     
     clock_gettime(CLOCK_MONOTONIC, &t1);
     
     if (taskid == MASTER) {
+        checkArgc(argc);
+        readArgv(argv, &n);
+        tasksz = n * n / numtasks;
+        allsz = n * n;
+        
+        double *A = malloc(n * n * sizeof(double));
+        double *B = malloc(n * n * sizeof(double));
+        double *C = malloc(n * n * sizeof(double));
+        
+        
         initMatrix(n, &A, &B);
+        
+        clock_gettime(CLOCK_MONOTONIC, &t1);
         for (tid = 1; tid < numtasks; tid++) {
+            MPI_Send(&allsz, 1, MPI_INT, tid, 1, MPI_COMM_WORLD);
             MPI_Send(&tasksz, 1, MPI_INT, tid, 1, MPI_COMM_WORLD);
             MPI_Send(&B, allsz, MPI_DOUBLE, tid, 1, MPI_COMM_WORLD);
             MPI_Send(&A[tid * tasksz], tasksz, MPI_DOUBLE, tid, 1, MPI_COMM_WORLD);
         }
         
-        matrixMultiplicationIKJ(n,A,B,&C,MASTER,numtasks);
-        
         for (tid = 1; tid < numtasks; tid++) {
-            MPI_Recv(&C, tasksz, MPI_DOUBLE, tid, 2, MPI_COMM_WORLD, &status);
+            MPI_Recv(&C[tid * tasksz], tasksz, MPI_DOUBLE, tid, 2, MPI_COMM_WORLD, &status);
         }
         
         clock_gettime(CLOCK_MONOTONIC, &t2);
+        time_sec = (double)(t2.tv_sec - t1.tv_sec);
+        time_nsec = (double)(t2.tv_nsec - t1.tv_nsec);
+        time_pass = time_sec + (time_nsec / 1000000000);
+        displayResult(time_pass, n, A, B, C);
+    
+        free(A);
+        free(B);
+        free(C);
     }else {
+
+        MPI_Recv(&allsz, 1, MPI_INT, MASTER, 2, MPI_COMM_WORLD, &status);
+        MPI_Recv(&tasksz, 1, MPI_INT, MASTER, 2, MPI_COMM_WORLD, &status);
+        
+        double *A = malloc(n * n * sizeof(double) / tasksz);
+        double *B = malloc(n * n * sizeof(double));
+        double *C = malloc(n * n * sizeof(double) / tasksz);
+        
         MPI_Recv(&B, allsz, MPI_DOUBLE, MASTER, 2, MPI_COMM_WORLD, &status);
         MPI_Recv(&A[taskid * tasksz], tasksz, MPI_DOUBLE, MASTER, 2, MPI_COMM_WORLD, &status);
         matrixMultiplicationIKJ(n,A,B,&C,taskid,numtasks);
         MPI_Send(&C, tasksz, MPI_DOUBLE, taskid, 1, MPI_COMM_WORLD);
+        
     }
-    
+    MPI_Finalize();
     //signals
     //MPI reduce??
-    MPI_Finalize();
     
-    time_sec = (double)(t2.tv_sec - t1.tv_sec);
-    time_nsec = (double)(t2.tv_nsec - t1.tv_nsec);
-    time_pass = time_sec + (time_nsec / 1000000000);
-    displayResult(time_pass, n, A, B, C);
-    
-    free(A);
-    free(B);
-    free(C);
     return 0;
 }
