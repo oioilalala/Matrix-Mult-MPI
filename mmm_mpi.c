008d67050a7529532bde72e7552d7e760fba59b1
oioilalala <35413266+oioilalala@users.noreply.github.com>
2018-11-11 22:27:02 -0800

    Update mmm_mpi.c

--- mmm_mpi.c
+++ mmm_mpi.c
@@ -76,7 +76,7 @@ int main(int argc, char **argv) {
    // double time_pass, time_sec, time_nsec;
     
     MPI_Init(&argc,&argv);
-    MPI_Status status;
+ //   MPI_Status status;
     MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
      
