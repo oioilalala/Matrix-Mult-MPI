6a8abd7d4c9985c25bea54ac274824bf9203df62
oioilalala <35413266+oioilalala@users.noreply.github.com>
2018-11-11 16:46:24 -0800

    Update mmm_mpi.c

--- mmm_mpi.c
+++ mmm_mpi.c
@@ -70,7 +70,8 @@ void displayResult(double time, int N, double *A, double *B, double *C) {
 
 int main(int argc, char **argv) {
     
-    int n, numtasks, taskid, tasksz, tid, allsz;
+    //int n, numtasks, taskid, tasksz, tid, allsz;
+    int numtasks, taskid;
     struct timespec t1, t2;
     double time_pass, time_sec, time_nsec;
     
