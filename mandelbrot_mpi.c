f70ec3c8709403cda1e25f8df71b50c403de5296
kluong97 <kluong97@ad3.ucdavis.edu@pc2.cs.ucdavis.edu>
2018-11-14 01:57:13 -0800

    Recv throws an error relating to buffer size/truncation

--- mandelbrot_mpi.c
+++ mandelbrot_mpi.c
@@ -5,7 +5,7 @@
 #include <mpi.h>
 #include <complex.h>
 
-#define AXIS_LEN 1024
+#define RES 1024
 #define BILLION 1e9
 #define MASTER 0
 
@@ -56,7 +56,7 @@ void getInput(int argc, char *argv[], double *x_cen, double *y_cen, int *zoom,
 
 int main(int argc, char*argv[]){
     double x_center, y_center, dist;
-    int zoom, cutoff, *plane, comm_size, comm_rank, section;
+    int zoom, cutoff, comm_size, comm_rank, section;
     struct timespec start, finish;
 
     // Initializing mpi
@@ -64,23 +64,19 @@ int main(int argc, char*argv[]){
     MPI_Status status;
     MPI_Comm_size(MPI_COMM_WORLD,&comm_size);
     MPI_Comm_rank(MPI_COMM_WORLD,&comm_rank);
-    if (comm_size != 2) {
-        if (!comm_rank)
-            MPI_Abort(MPI_COMM_WORLD, 1);
-    }
 
 
     if(MASTER == comm_rank){        // Master code
         getInput(argc, argv, &x_center, &y_center, &zoom, &cutoff);
 
+        int *plane = malloc(sizeof(int) * RES * RES);
         dist = pow(2, -1 * zoom);
 
-        section = AXIS_LEN / (comm_size - 1);
+        section = RES / (comm_size - 1);
+        /*
         if (0 < (AXIS_LEN % (comm_size - 1))){      // Remainder exists, so round up 1
             section += 1;
-        }
-
-        plane = malloc(sizeof(int) * AXIS_LEN * AXIS_LEN);
+        }*/
 
         clock_gettime(CLOCK_REALTIME, &start);
         for(int tid = 1; tid < comm_size; tid++){
@@ -89,16 +85,17 @@ int main(int argc, char*argv[]){
             MPI_Send(&dist, 1, MPI_DOUBLE, tid, 1, MPI_COMM_WORLD);
             MPI_Send(&cutoff, 1, MPI_DOUBLE, tid, 1, MPI_COMM_WORLD);
             MPI_Send(&section, 1, MPI_INT, tid, 1, MPI_COMM_WORLD);
-            MPI_Send(&plane, AXIS_LEN * AXIS_LEN, MPI_INT, tid, 1, MPI_COMM_WORLD);
+            MPI_Send(&plane[0], RES * RES, MPI_INT, tid, 1, MPI_COMM_WORLD);
         }
 
         for(int tid = 1; tid < comm_size; tid++){
-            if(AXIS_LEN < tid * section){           // Only populates within the dimensions of the matrix
-                MPI_Recv(&plane[(tid - 1) * section], section - ((tid * section) - AXIS_LEN),
-                         MPI_INT, tid, 1, MPI_COMM_WORLD, &status);
+            if(RES < tid * section){           // Only populates within the dimensions of the matrix
+                MPI_Recv(&plane[(tid - 1) * section],
+                         section - ((tid * section) - RES),
+                         MPI_INT, tid, 2, MPI_COMM_WORLD, &status);
             }
             else {
-                MPI_Recv(&plane[(tid - 1) * section], section, MPI_INT, tid, 1,
+                MPI_Recv(&plane[(tid - 1) * section], section, MPI_INT, tid, 2,
                          MPI_COMM_WORLD, &status);
             }
         }
@@ -107,7 +104,7 @@ int main(int argc, char*argv[]){
         double time = (finish.tv_sec - start.tv_sec) +
                       (finish.tv_nsec - start.tv_nsec) / BILLION;
         printf("Runtime: %f\n", time);
-        printf("M: %u\n", matrix_checksum(AXIS_LEN, plane, sizeof(int)));
+        printf("M: %u\n", matrix_checksum(RES, plane, sizeof(int)));
 
         free(plane);
     }
@@ -117,17 +114,20 @@ int main(int argc, char*argv[]){
         MPI_Recv(&dist, 1, MPI_DOUBLE, MASTER, 1, MPI_COMM_WORLD, &status);
         MPI_Recv(&cutoff, 1, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
         MPI_Recv(&section, 1, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&plane, AXIS_LEN * AXIS_LEN, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
+
+        int *plane = malloc(sizeof(int) * RES * RES);
+
+        MPI_Recv(&plane[0], RES * RES, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
 
         int start = (comm_rank - 1) * section,
             end = (start + section) - 1;
 
         for(int i = start; i < end; i++){
-            for(int j = 0; j < AXIS_LEN; j++){
-                plane[i * AXIS_LEN + j] = iteratePoint(dist * j + x_center, dist * i + y_center, cutoff);
+            for(int j = 0; j < RES; j++){
+                plane[i * RES + j] = iteratePoint(dist * j + x_center, dist * i + y_center, cutoff);
             }
         }
-        MPI_Send(&plane[start], AXIS_LEN * section, MPI_INT, MASTER, 2, MPI_COMM_WORLD);
+        MPI_Send(&plane[start], RES * section, MPI_INT, MASTER, 2, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
