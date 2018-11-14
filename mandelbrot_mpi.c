ac45d165c89f3722b2860ca137cb158427541506
Keith <keithluong42@gmail.com>
2018-11-13 21:19:28 -0800

    Added mpi implementation, but bugged. Outputs multiple times with the same runtime as serial.

--- mandelbrot_mpi.c
+++ mandelbrot_mpi.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <mpi.h>
 #include <complex.h>
 
 #define AXIS_LEN 1024
 #define BILLION 1e9
+#define MASTER 0
 
 unsigned int matrix_checksum(int N, void *M, unsigned int size);
 
@@ -22,60 +24,113 @@ int iteratePoint(double x, double y, int cutoff){
     return i;
 }
 
-void calcPlane(int *plane, int x_center, int y_center, int zoom, int cutoff){
-    struct timespec start, finish;
-    double dist = pow(2, -1 * zoom);
-
-    // Iterate through the 1024x1024 points in the map, passing x and y
-    // to iteratePoint() to determine Mandelbrot membership for each
-    clock_gettime(CLOCK_REALTIME, &start);
-    for(int i = 0; i < AXIS_LEN; i++){
-        for(int j = 0; j < AXIS_LEN; j++){
-            plane[i * AXIS_LEN + j] = iteratePoint(dist * j, dist * i, cutoff);
-        }
-    }
-    clock_gettime(CLOCK_REALTIME, &finish);
-
-    double time = (finish.tv_sec - start.tv_sec) +
-                  (finish.tv_nsec - start.tv_nsec) / BILLION;
-
-    printf("Runtime: %f\n", time);
-    printf("M: %u\n", matrix_checksum(AXIS_LEN, plane, sizeof(int)));
-}
-
-int main(int argc, char*argv[]){
-    double x_center, y_center;
-    int zoom, cutoff, *plane;
+void getInput(int argc, char *argv[], double *x_cen, double *y_cen, int *zoom,
+                int *cutoff){
+    *x_cen = atof(argv[1]);
+    *y_cen = atof(argv[2]);
+    *zoom = atoi(argv[3]);
+    *cutoff = atoi(argv[4]);
 
     if(5 != argc){
         fprintf(stderr, "Usage: %s xcenter ycenter zoom cutoff", argv[0]);
-        return 1;
+        exit(1);
     }
-
     // Get and validate arguments
-    x_center = atof(argv[1]);
-    y_center = atof(argv[2]);
-    zoom = atoi(argv[3]);
-    cutoff = atoi(argv[4]);
-
-    if(10 < x_center || -10 > x_center){
+    if(10 < *x_cen || -10 > *x_cen){
         fprintf(stderr, "Error: wrong x-center (-10.000000 <= N <= 10.000000)\n");
-        return 1;
+        exit(1);
     }
-    if(10 < y_center || -10 > y_center) {
+    if(10 < *y_cen || -10 > *y_cen) {
         fprintf(stderr, "Error: wrong y-center (-10.000000 <= N <= 10.000000)\n");
-        return 1;
+        exit(1);
     }
-    if(100 < zoom || 0 > zoom){
+    if(100 < *zoom || 0 > *zoom){
         fprintf(stderr, "Error: wrong zoom (0 <= N <= 100)\n");
-        return 1;
+        exit(1);
     }
-    if(1000 < cutoff || 50 > cutoff){
+    if(1000 < *cutoff || 50 > *cutoff){
         fprintf(stderr, "Error: wrong cutoff (50 <= N <= 1000)");
-        return 1;
+        exit(1);
+    }
+}
+
+int main(int argc, char*argv[]){
+    double x_center, y_center, dist;
+    int zoom, cutoff, *plane, comm_size, comm_rank, section;
+    struct timespec start, finish;
+
+    // Initializing mpi
+    MPI_Init(&argc,&argv);
+    MPI_Status status;
+    MPI_Comm_size(MPI_COMM_WORLD,&comm_size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&comm_rank);
+    if (comm_size != 2) {
+        if (!comm_rank)
+            MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    plane = malloc(sizeof(int) * AXIS_LEN * AXIS_LEN);
 
-    calcPlane(plane, x_center, y_center, zoom, cutoff);
+    if(MASTER == comm_rank){        // Master code
+        getInput(argc, argv, &x_center, &y_center, &zoom, &cutoff);
+
+        dist = pow(2, -1 * zoom);
+
+        section = AXIS_LEN / (comm_size - 1);
+        if (0 < (AXIS_LEN % (comm_size - 1))){      // Remainder exists, so round up 1
+            section += 1;
+        }
+
+        plane = malloc(sizeof(int) * AXIS_LEN * AXIS_LEN);
+
+        clock_gettime(CLOCK_REALTIME, &start);
+        for(int tid = 1; tid < comm_size; tid++){
+            MPI_Send(&x_center, 1, MPI_DOUBLE, tid, 1, MPI_COMM_WORLD);
+            MPI_Send(&y_center, 1, MPI_DOUBLE, tid, 1, MPI_COMM_WORLD);
+            MPI_Send(&dist, 1, MPI_DOUBLE, tid, 1, MPI_COMM_WORLD);
+            MPI_Send(&cutoff, 1, MPI_DOUBLE, tid, 1, MPI_COMM_WORLD);
+            MPI_Send(&section, 1, MPI_INT, tid, 1, MPI_COMM_WORLD);
+            MPI_Send(&plane, AXIS_LEN * AXIS_LEN, MPI_INT, tid, 1, MPI_COMM_WORLD);
+        }
+
+        for(int tid = 1; tid < comm_size; tid++){
+            if(AXIS_LEN < tid * section){           // Only populates within the dimensions of the matrix
+                MPI_Recv(&plane[(tid - 1) * section], section - ((tid * section) - AXIS_LEN),
+                         MPI_INT, tid, 1, MPI_COMM_WORLD, &status);
+            }
+            else {
+                MPI_Recv(&plane[(tid - 1) * section], section, MPI_INT, tid, 1,
+                         MPI_COMM_WORLD, &status);
+            }
+        }
+        clock_gettime(CLOCK_REALTIME, &finish);
+
+        double time = (finish.tv_sec - start.tv_sec) +
+                      (finish.tv_nsec - start.tv_nsec) / BILLION;
+        printf("Runtime: %f\n", time);
+        printf("M: %u\n", matrix_checksum(AXIS_LEN, plane, sizeof(int)));
+
+        free(plane);
+    }
+    else{       // Slave code
+        MPI_Recv(&x_center, 1, MPI_DOUBLE, MASTER, 1, MPI_COMM_WORLD, &status);         // Receive parameters from master
+        MPI_Recv(&y_center, 1, MPI_DOUBLE, MASTER, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&dist, 1, MPI_DOUBLE, MASTER, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&cutoff, 1, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&section, 1, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&plane, AXIS_LEN * AXIS_LEN, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
+
+        int start = (comm_rank - 1) * section,
+            end = (start + section) - 1;
+
+        for(int i = start; i < end; i++){
+            for(int j = 0; j < AXIS_LEN; j++){
+                plane[i * AXIS_LEN + j] = iteratePoint(dist * j + x_center, dist * i + y_center, cutoff);
+            }
+        }
+        MPI_Send(&plane[start], AXIS_LEN * section, MPI_INT, MASTER, 2, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+
+    return 0;
 }
